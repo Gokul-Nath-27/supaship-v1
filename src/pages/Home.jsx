082301d6190662e2344a49b2a92d88f42c50605e
Prathiksha <prathikshavijay08@gmail.com>
2023-04-15 21:45:09 +0530

    Login system has been completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/home.css";
+import supabase from "../config/client";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
 
-export default Home
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        await supabase.auth.getUser().then((response) => {
+          console.log(response);
+          setUser(response?.data?.user);
+        });
+      } catch (err) {
+        console.log(err);
+        alert(err);
+      }
+    };
+    getUserData();
+  }, []);
+
+  const signOutUser = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.log("Error signing out:", error.message);
+    } else {
+      navigate("/");
+    }
+  };
+
+  return (
+    <div className="home">
+      <button onClick={ () => signOutUser()}>Sign Out</button>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import { Auth } from '@supabase/auth-ui-react'
-import { useNavigate } from 'react-router-dom'
-
-import supabase from '../config/client'
-
+import React from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useNavigate } from "react-router-dom";
+import supabase from "../config/client";
+import '../styles/login.css'
 const Login = () => {
-  const navigate = useNavigate()
-  console.log(supabase)
-  return (
-    <div>
-        Login
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Login
+  supabase.auth.onAuthStateChange(async (event) => {
+    if (event !== "SIGNED_OUT") {
+      navigate("/home");
+    } else {
+      navigate("/");
+    }
+  });
+
+  return (
+    <div className="login">
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={["google", "discord"]}
+        theme="dark"
+      />
+    </div>
+  );
+};
+
+export default Login;
